65d26809261becbff939a428bc7454e0b822d7f3
skolli1510 <skolli8@charlotte.edu>
2024-11-12 04:35:41 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const login = async (credentials) => {
-    const response = await axios.post('http://localhost:5000/api/login', credentials);
+    const response = await axios.post('https://nbad-s62-15.onrender.com/api/login', credentials);
   return response.data;
 };
